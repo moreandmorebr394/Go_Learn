bb2b9cda165b0630dab54f8a7e785a411e6edce0
Bruna Alve <130995251+moreandmorebr394@users.noreply.github.com>
2023-10-05 10:24:54 -0300

    Update De2.go

--- De2.go
+++ De2.go
@@ -6,11 +6,11 @@ func main() {
 
 	for i := 1; i <= 100; i++ {
 		if i%3 == 0 && i%5 == 0 {
-			fmt.Println("PingPang")
+			fmt.Println("Ping Pong")
 		} else if i%3 == 0 {
 			fmt.Println("Ping")
 		} else if i%5 == 0 {
-			fmt.Println("Pang")
+			fmt.Println("Pong")
 		} else {
 			fmt.Println(i)
 
