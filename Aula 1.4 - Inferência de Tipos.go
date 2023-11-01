ece2dcf189ab82eb09f2d97395440d8a2ff97a6a
Bruna Alve <130995251+moreandmorebr394@users.noreply.github.com>
2023-11-01 19:14:22 -0300

    Create Aula 1.4 - Inferência de Tipos.go

---
+++ Aula 1.4 - Inferência de Tipos.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+  var nome string = "Alves"
+  var idade int = 23
+  var versao float 32 = 2.2
+  fmt.Println("Olá sr.", nome, "sua idade é", idade)
+  fmt.Println("Este programa está na versão", versao)
+  
+}
