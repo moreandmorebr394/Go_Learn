8f8e7bbcfdf913cb23714a93f3f599cf187cc503
Bruna Alve <130995251+moreandmorebr394@users.noreply.github.com>
2023-11-20 12:12:09 -0300

    Create Aula 2.2 - Operadores Curtos.go

---
+++ Aula 2.2 - Operadores Curtos.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+const ebulicaof float64 = 211.8
+
+func main(){
+
+  var tempf := ebulicaof
+  var tempc := (F - 32)*5/9
+  fmt.Println("A temperatura de ebuliçao da água em F = ",tempf)
+  fmt.Println("A temperatura de ebuliçaõ da água em C = ",tempc)
+  }
