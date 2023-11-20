64afa8738398c9dc2aedba751fabee2247813fb5
Bruna Alve <130995251+moreandmorebr394@users.noreply.github.com>
2023-11-20 12:16:49 -0300

    Create Aula 2.3 - Printl x Printf.go

---
+++ Aula 2.3 - Printl x Printf.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+const ebulicaof float64 = 211.8
+
+func main(){
+
+  var tempf float64 = ebulicaof
+  var tempc float64 = (F - 32)*5/9
+  fmt.Printf("A temperatura de ebuliçao da água em F =    e a temperatura de ebuliçaõ da água em C = ",tempf, tempc)
+  }
